9086b8210165908284cf61a8681b9fd772d46d86
mohammad2745 <mohammadfcc2745@gmail.com>
2023-03-13 20:09:16 +0600

    Circle Drawing

---
+++ Shapes/14 - drawing circular objects/canvas-script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+    // Definitions
+    var canvas = document.getElementById("circular-objects-canvas");
+    var context = canvas.getContext("2d");
+
+    var radian = Math.PI / 180;
+
+    // context.arc(centerX, centerY, radius, startAngle, endAngle, antiClockwiseDirection);
+
+    context.beginPath();
+    context.strokeStyle = "orange";
+    context.lineWidth = 15;
+    // context.fillStyle = "orange";
+    // context.arc(450, 300, 150, 135 * radian, 360 * radian, false);
+    context.fillStyle = "red";
+    context.arc(450, 300, 150, 135 * radian, 360 * radian, true);
+    context.stroke();
+    context.fill();
+
+
+}
