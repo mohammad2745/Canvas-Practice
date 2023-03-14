ff0387c7d8dd703b758906ae80519564fc5227ed
mohammad2745 <mohammadfcc2745@gmail.com>
2023-03-14 11:47:43 +0600

    Text

---
+++ Texts/18 - drawing basic texts/canvas-script.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+
+    // Definitions
+    var canvas = document.getElementById("basic-texts-canvas");
+    var context = canvas.getContext("2d");
+
+
+    context.font = "45px Verdana";
+
+    // context.fillText(text, xCoord, yCoord, maxWidth);
+
+    var fText = "Fill Text on Canvas";
+
+    context.fillText(fText, 100, 100)
+
+
+    // context.strokeText(text, xCoord, yCoord, maxWidth);
+
+    var sText = "Stroke Text on Canvas";
+
+    context.strokeText(sText, 80, 200);
+
+
+
+
+
+
+}
